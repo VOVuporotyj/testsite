ecc1c547a6332bf78788833dae6e3b5d8003c87f
VOVuporotyj <Vladimir59912@gmail.com>
2017-03-20 17:10:22 +0300

    жизнь боль

--- main.go
+++ main.go
@@ -3,28 +3,97 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/VOVuporotyj/testsite/Reg"
+	"github.com/boltdb/bolt"
 	"github.com/go-martini/martini"
 )
 
-func indexPage(w http.ResponseWriter, req *http.Request) {
-	t, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-	}
-	fmt.Println(t.ExecuteTemplate(w, "indexPage", nil))
+var templates = template.Must(template.ParseGlob("templates/*"))
 
+func indexPage(w http.ResponseWriter, req *http.Request) {
+	templates.ExecuteTemplate(w, "indexPage", nil)
+}
+
+//RegisPage открывает шаблон regis.
+func RegisPage(w http.ResponseWriter, req *http.Request) {
+	templates.ExecuteTemplate(w, "regisPage", nil)
+}
+
+//PostRegis записывает в БД лог и пас.
+func PostRegis(w http.ResponseWriter, r *http.Request) {
+	username := r.PostFormValue("username")
+	password := r.PostFormValue("password")
+	fmt.Println(username)
+	fmt.Println(password)
+
+	db, err := bolt.Open("DB/test.db", 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(db.Path())
+
+	if err := db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte("Login"))
+		if err != nil {
+			return err
+		}
+		if err := b.Put([]byte(username), []byte(password)); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
+	templates.ExecuteTemplate(w, "loginPage", nil)
+}
+
+//LoginPage открывает шаблон Login.
+func LoginPage(w http.ResponseWriter, req *http.Request) {
+	templates.ExecuteTemplate(w, "loginPage", nil)
+}
+
+//Postlogin сравнивает логин и пароль с базой.
+func Postlogin(w http.ResponseWriter, r *http.Request) {
+	username := r.PostFormValue("username")
+	password := r.PostFormValue("password")
+	fmt.Println(username)
+	fmt.Println(password)
+	db, err := bolt.Open("DB/test.db", 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(db.Path())
+
+	if err := db.View(func(tx *bolt.Tx) error {
+		v := tx.Bucket([]byte("login")).Get([]byte("username"))
+		if string(v) == username {
+			templates.ExecuteTemplate(w, "testPage", nil)
+		}
+
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	templates.ExecuteTemplate(w, "testPage", nil)
 }
 
 func main() {
 
 	m := martini.Classic()
 	m.Get("/", indexPage)
-	m.Get("/regis", Reg.RegisPage)
-	m.Post("/testPage", Reg.PostRegis)
-	m.Get("/login", Reg.LoginPage)
-	m.Post("/testPage", Reg.Postlogin)
+	m.Get("/regis", RegisPage)
+	m.Post("/testPage", PostRegis)
+	m.Get("/login", LoginPage)
+	m.Post("/testPage", Postlogin)
 	m.Run()
 }
